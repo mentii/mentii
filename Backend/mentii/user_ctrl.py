c0f40caeacd52543bfc1f42665ed005ae0a8f8fb
Alex Palmatier <asp78@drexel.edu>
2016-12-12 16:17:30 -0500

    refactoring user_ctrl

--- Backend/mentii/user_ctrl.py
+++ Backend/mentii/user_ctrl.py
@@ -5,103 +5,111 @@ from boto3.dynamodb.conditions import Key, Attr
 import uuid
 
 def register(jsonData, mailer):
-	if not validateRegistration(jsonData):
-		return 'Failing Registration Validation'
-	return addUser(getEmail(jsonData), getPassword(jsonData), mailer)
+  if validateRegistrationJSON(jsonData):
+    email = parseEmail(jsonData)
+    activationId = addUserAndSendEmail(email, parsePassword(jsonData), mailer)
+    return activationId
+  else:
+    return 'Failing Registration Validation'
 
-def validateRegistration(jsonData):
-    '''
-    Validate the email address and password
-    for a user.
-    '''
-    try:
-        email = getEmail(jsonData)
-        #Emails must have only one @ and at least one . after the @
-        emailRegex = re.compile(r"[^@]+@[^@]+\.[^@]+")
-        if not emailRegex.match(email):
-            return False
+def validateRegistrationJSON(jsonData):
+  '''
+  Validate that the JSON object contains
+  an email and password attributes
+  '''
+  try:
+    email = parseEmail(jsonData)
+    password = parsePassword(jsonData)
+  except Exception as e:
+    print e
+    return False
+  
+  return True
 
-        if isEmailInSystem(email):
-            return False
+def parseEmail(jsonData):
+  return jsonData['email']
 
-        password = getPassword(jsonData)
-        #Passwords must be at least 8 characters
-        if len(password) < 8:
-            return False
+def parsePassword(jsonData):
+  return jsonData['password']
 
-        return True
-    except Exception as e:
-        print e
-        return False
+def isEmailValid(email):
+  '''
+  Validate that thee email is matches the
+  format required.
+  '''
+  emailRegex = re.compile(r"[^@]+@[^@]+\.[^@]+")
+  return emailRegex.match(email)
 
-def getEmail(jsonData):
-    return jsonData['email']
+def isPasswordValid(password):
+  return len(password) >= 8
 
-def getPassword(jsonData):
-    return jsonData['password']
-
-def addUser(email, password, mailer):
-	activationId = str(uuid.uuid4())
-	dynamodb = boto3.resource('dynamodb')
-	table = dynamodb.Table('users')
-	#this will change an existing user withthe same email.
-	#TODO:we need enforce uniqueness on email
-	response = table.put_item(
-		Item={
-			'email': email,
-			'password': password,
-			'activationId': activationId
-		}
-	)
-	#TODO:send email with activation link here
-	#currently this is returning the activationId for testing but once the email
-	#is working it should just return a success message
-        sendEmail(email, activationId, mailer)
-	return activationId
-
-def sendEmail(email, activationId, mailer):
-    '''
-    Create a message and send it from our email to
-    the passed in email. The message should contain
-    a link built with the activationId
-    '''
-    #Build Message
-    msg = Message('Mentii: Thank You for Creating an Account!', recipients=[email])
-    msg.body = "Here is your activationId link: api.mentii.me/activate/{}".format(activationId)
-    #Send Email
-    mailer.send(msg)
-
-def isEmailInSystem(email):
+def addUserAndSendEmail(email, password, mailer):
+  if isEmailValid(email) and isPasswordValid(password) and not isEmailInSystem(email):
+    activationId = str(uuid.uuid4())
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('users')
-    result = table.get_item(Key={'email': email})
-    #Result is a dictionary that will have the key Item if
-    # it was able to find an item.
-    return "Item" in result.keys()
 
+    #This will change an existing user with the same email.
+    response = table.put_item(
+      Item={
+        'email': email,
+        'password': password,
+        'activationId': activationId
+      }
+    )
+    sendEmail(email, activationId, mailer)
+    return activationId
+  else:
+    return 'none'
+
+def sendEmail(email, activationId, mailer):
+  '''
+  Create a message and send it from our email to
+  the passed in email. The message should contain
+  a link built with the activationId
+  '''
+  #Build Message
+  msg = Message('Mentii: Thank You for Creating an Account!', recipients=[email])
+  msg.body = "Here is your activationId link: api.mentii.me/activate/{}".format(activationId)
+
+  #Send Email
+  mailer.send(msg)
+
+def isEmailInSystem(email):
+  dynamodb = boto3.resource('dynamodb')
+  table = dynamodb.Table('users')
+
+  #Result is a dictionary that will have the key Item if
+  # it was able to find an item.
+  result = table.get_item(Key={'email': email})
+  return "Item" in result.keys()
 
 def activate(activationId):
-	dynamodb = boto3.resource('dynamodb')
-	table = dynamodb.Table('users')
-        email = ''
-        #Scan for the email associated with this activationId
-        scanResponse = table.scan(FilterExpression=Attr('activationId').eq(activationId))
-        if scanResponse is not None:
-            #scanResponse is a dictionary that has a list of 'Items'
-            items = scanResponse['Items']
-            if len(items) != 1:
-                return "Error!! Could not find an item with that code."
-            email = items[0]['email']
-        #Update using the email we have
-	response = table.update_item(
-	    Key={
-	        'email': email,
-	    },
-	    UpdateExpression="SET active = :a, activationId = :aid",
-	    ExpressionAttributeValues={
-	        ':a': 'T',
-	        ':aid': 'none'
-	    },
-	    ReturnValues='UPDATED_NEW'
-	)
-	return "Success"
+  dynamodb = boto3.resource('dynamodb')
+  table = dynamodb.Table('users')
+
+  #Scan for the email associated with this activationId
+  scanResponse = table.scan(FilterExpression=Attr('activationId').eq(activationId))
+
+  if scanResponse is not None:
+    #scanResponse is a dictionary that has a list of 'Items'
+    items = scanResponse['Items']
+
+  if len(items) != 1:
+    return "Error!! Could not find an item with that code."
+  else:
+    email = items[0]['email']
+
+    #Update using the email we have
+    response = table.update_item(
+    Key={
+        'email': email,
+      },
+      UpdateExpression="SET active = :a, activationId = :aid",
+      ExpressionAttributeValues={
+        ':a': 'T',
+        ':aid': 'none'
+      },
+      ReturnValues='UPDATED_NEW'
+    )
+    return "Success"

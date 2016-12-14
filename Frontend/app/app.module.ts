4754ba1298c7f0607265cfc0e5e8a3cb09e2f6fc
Aaron Segal <aaronmsegal@gmail.com>
2016-12-14 14:31:48 -0500

    all reg validation working

--- Frontend/app/app.module.ts
+++ Frontend/app/app.module.ts
@@ -7,10 +7,11 @@ import { RegistrationComponent }   from './registration/registration.component';
 import { RootComponent }   from './root/root.component';
 import { PageNotFoundComponent }   from './pageNotFound/pageNotFound.component';
 import { routing } from './app.routes';
+import { EqualValidator } from './directives/equal-validator.directive';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpModule, routing],
-	declarations: [ AppComponent, RegistrationComponent, RootComponent, PageNotFoundComponent ],
+	declarations: [ AppComponent, RegistrationComponent, RootComponent, PageNotFoundComponent, EqualValidator],
 	bootstrap:    [ AppComponent ]
 })
 export class AppModule { }

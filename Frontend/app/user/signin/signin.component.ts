06525b2205c35c1a4b107122cfe3ff22254fc3ca
Aaron Segal <aaronmsegal@gmail.com>
2016-12-26 14:14:35 -0500

    Service methods for Auth Token

--- Frontend/app/user/signin/signin.component.ts
+++ Frontend/app/user/signin/signin.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { SigninModel } from './signin.model';
 import { MentiiConfig } from '../../mentii.config';
 import { UserService } from '../user.service';
+import { AuthHttp } from '../../utils/AuthHttp.service';
 
 @Component({
   moduleId: module.id,
@@ -14,13 +15,12 @@ export class SigninComponent {
   model = new SigninModel('', '');
   mentiiConfig = new MentiiConfig();
 
-  constructor(public userService: UserService, public router: Router){
+  constructor(public userService: UserService, public authHttpService: AuthHttp , public router: Router){
   }
 
   handleSuccess(data) {
     if (data.payload.token) {
-      // TODO: Store the auth token somewhere else
-      localStorage.setItem("auth_token", data.payload.token);
+      this.authHttpService.saveAuthToken(data.payload.token);
       this.router.navigateByUrl('/secure-test');
     } else {
       alert("Success but no token. False authentication");

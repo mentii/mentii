305350c61ce3a61d70390637a141e50c85d86710
Jonathan McDaniel <jonathantmcdaniel@gmail.com>
2017-01-04 16:03:48 -0500

    init commit of class list

--- Frontend/app/root/root.component.ts
+++ Frontend/app/root/root.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SigninComponent } from '../user/signin/signin.component'
+import { AuthHttp } from '../utils/AuthHttp.service';
 
 @Component({
   moduleId: module.id,
@@ -7,7 +8,17 @@ import { SigninComponent } from '../user/signin/signin.component'
   templateUrl: 'root.html'
 })
 export class RootComponent {
+  isUserAuthenticated = false;
 
-  constructor( ){
+  constructor( public authHttp: AuthHttp){
+    this.checkAuthToken();
+  }
+
+  checkAuthToken() {
+    if (this.authHttp.loadAuthToken() == null) {
+      this.isUserAuthenticated = false;
+    } else {
+      this.isUserAuthenticated = true;
+    }
   }
 }

8376f71072dbf44f7c93231f448b2a310c9d9d56
Ryan <ryan@rdyoung.us>
2017-04-12 12:57:52 -0400

    Choice based off of probability working

--- Backend/mentii/problem_ctrl.py
+++ Backend/mentii/problem_ctrl.py
@@ -1,6 +1,7 @@
 from utils import db_utils as dbUtils
 import utils.MentiiLogging as MentiiLogging
 import random
+from numpy.random import choice
 import book_ctrl
 
 
@@ -33,8 +34,33 @@ def getBookInfoFromActivity(classId, activityTitle, dynamoDBInstance):
 
   return (bookId, chapterTitle, sectionTitle)
 
+def chooseProblemTemplate(templateList, userHistoryList):
+  '''
+  The userHistoryList should be a list of integers
+  '''
+  problemTemplate = 'Bad Problem'
+  if len(templateList) == 0:
+    print("error, empty template list passed")
+  else:
+    history = [-1*x for x in userHistoryList]
+    if len(history) != len(templateList):
+      history = [ 0 for _ in xrange(len(templateList))]
 
-def getProblemFromBook(bookId, chapterTitle, sectionTitle, dynamoDBInstance):
+    #Normalize the history to be all positive numbers
+    minVal = min(history)
+    history = [x + abs(minVal) + 1 for x in history]
+    #Create a probablility distrobution
+    total = sum(history)
+    probDist = [float(x)/total for x in history]
+    print(history)
+    print(probDist)
+
+    problemTemplate = choice(templateList, p=probDist)
+
+  return problemTemplate
+
+
+def getProblemFromBook(bookId, chapterTitle, sectionTitle, userId, dynamoDBInstance):
   #Get the book
   book = book_ctrl.getBook(bookId, dynamoDBInstance)
   problem = 'Bad Problem'
@@ -44,6 +70,7 @@ def getProblemFromBook(bookId, chapterTitle, sectionTitle, dynamoDBInstance):
         sections = chapter.get('sections', [])
         for section in sections:
           if section.get('title', '') == sectionTitle:
+            #Get the user list 
             problem = random.choice(section['problems']).get('problemString')
             break #Break out of section loop
         break #Break out of chapter loop
